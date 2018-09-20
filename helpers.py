709d9e520e01e25c7b78c64510f19e1eb4473e27
Martin Eden <martin.s.eden@pm.me>
2018-09-20 14:42:08 +0100

    Add helper method

---
+++ helpers.py
@@ -0,0 +1,8 @@
+import os
+
+def save_securely(path, data, permissions=0o600):
+    with open(path, 'a'):  # Create file if does not exist
+        pass
+    os.chmod(path, permissions)
+    with open(path, 'w') as f:
+        f.write(data)
