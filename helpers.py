21336ebf747d29b79e2d4e5ff02e00161661688a
Martin Eden <martin.s.eden@pm.me>
2018-09-20 14:43:02 +0100

    Improve function name

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import os
 
-def save_securely(path, data, permissions=0o600):
+def save_secret_to_file(path, data, permissions=0o600):
     with open(path, 'a'):  # Create file if does not exist
         pass
     os.chmod(path, permissions)
